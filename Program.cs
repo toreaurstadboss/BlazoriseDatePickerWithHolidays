756c2d30c762ba6e374b10b4b5f4feca262abc81
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-05 22:41:01 +0200

    Updated to Target framework .NET 8. Started looking at Custom Date Picker with Blazorise with Highlighted Holidays

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazoriseDatePickerWithHolidays;
+using Blazorise;
+using Blazorise.Bootstrap5;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services 
+.AddBlazorise(options => {
+    options.Immediate = false; // Use immediate mode for Blazorise components
+})
+.AddBootstrap5Providers()
+.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
